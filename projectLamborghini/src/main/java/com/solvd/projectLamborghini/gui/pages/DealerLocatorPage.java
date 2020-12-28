4554f32862ae571200b84dc307fc1230d3557fee
Josh Corino <joshuc98@gmail.com>
2020-12-28 16:52:59 -0300

    Atributes of DealerLocatorPage added

--- projectLamborghini/src/main/java/com/solvd/projectLamborghini/gui/pages/DealerLocatorPage.java
+++ projectLamborghini/src/main/java/com/solvd/projectLamborghini/gui/pages/DealerLocatorPage.java
@@ -1,5 +1,28 @@
 package com.solvd.projectLamborghini.gui.pages;
 
-public class DealerLocatorPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+
+public class DealerLocatorPage extends LamborghiniAbstractPage{
+
+	
+	@FindBy(xpath="//*[@id=\"locator\"]/div[4]/div[1]/div[1]")
+	private ExtendedWebElement dealerTypes;
+	
+	@FindBy(xpath="//*[@id=\"locator\"]/div[4]/div[1]/div[2]")
+	private ExtendedWebElement searchBar;
+	
+	@FindBy(xpath="//*[@id=\"locator\"]/div[4]/div[2]")
+	private ExtendedWebElement results;
+	
+	@FindBy(id="map")
+	private ExtendedWebElement map;
+	
+	
+	public DealerLocatorPage(WebDriver driver) {
+		super(driver);
+	}
 
 }
